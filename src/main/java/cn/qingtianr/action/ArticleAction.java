2adc4e597ad140791c43100a44bc98090b8c99e7
yang2yang <197753119@qq.com>
2016-04-25 21:59:11 +0800

    自定义类型转换器，解决不同浏览器sql.Date类型显示不正确问题

--- src/main/java/cn/qingtianr/action/ArticleAction.java
+++ src/main/java/cn/qingtianr/action/ArticleAction.java
@@ -1,7 +1,6 @@
 package main.java.cn.qingtianr.action;
 
 import com.opensymphony.xwork2.ActionSupport;
-import main.java.cn.qingtianr.factory.ServiceFactory;
 import main.java.cn.qingtianr.model.Archive;
 import main.java.cn.qingtianr.model.ArchiveCount;
 import main.java.cn.qingtianr.model.Article;
